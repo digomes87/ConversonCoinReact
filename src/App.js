360df12a2f38620a514b749fdd88116d161aac22
Diego Go <diego.gomes87@gmail.com>
2019-08-20 17:56:26 -0300

    Starting project in React, conversor for coins

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+    </div>
+  );
+}
+
+export default App;
